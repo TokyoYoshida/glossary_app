f4658410604056cdd03071c0e438c35634df4b1d
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-03-28 14:37:12 +0900

    Signupフォームを作った

--- lib/view/auth_view.dart
+++ lib/view/auth_view.dart
@@ -53,19 +53,21 @@ class LoginScreen extends StatelessWidget {
   Future<String> _signupUser(LoginData data) {
     print('Name: ${data.name}, Password: ${data.password}');
     return CognitoService.auth(data.name, data.name, data.password)
-          .then((result) {
-        print("success!");
-        authMode = 2;
-        return null;
-      }).catchError((error) {
-        print("authviewerror!" + error.toString());
-        return extractErrorMessage(error.toString());
-      });
+        .then((result) {
+      print("success!");
+      authMode = 2;
+      return null;
+    }).catchError((error) {
+      print("authviewerror!" + error.toString());
+      return extractErrorMessage(error.toString());
+    });
   }
 
   String extractErrorMessage(String original) {
     final exp = RegExp(r'message: (.*)');
-    var list = exp?.allMatches(original).map((match) => match.group(1)).toList();
+    var list = exp?.allMatches(original)
+        .map((match) => match.group(1))
+        .toList();
     if (list.length == 1) {
       return list[0];
     }
@@ -86,21 +88,21 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<CounterStore>(
         builder: (context, counterStore, _) {
-      return FlutterLogin(
-        title: 'ECORP',
-        logo: 'assets/images/ecorp-lightblue.png',
-        onLogin: _authUser,
-        onSignup: _signupUser,
-        onSubmitAnimationCompleted: () {
-          if (authMode == 1) {
-            Navigator.of(context).pushNamed('/afterlogin');
-          } else {
-            Navigator.of(context).pushNamed('/aftersingup');
-          }
-        },
-        onRecoverPassword: _recoverPassword,
-      );
-    });
+          return FlutterLogin(
+            title: 'ECORP',
+            logo: 'assets/images/ecorp-lightblue.png',
+            onLogin: _authUser,
+            onSignup: _signupUser,
+            onSubmitAnimationCompleted: () {
+              if (authMode == 1) {
+                Navigator.of(context).pushNamed('/afterlogin');
+              } else {
+                Navigator.of(context).pushNamed('/aftersingup');
+              }
+            },
+            onRecoverPassword: _recoverPassword,
+          );
+        });
   }
 }
 
@@ -114,12 +116,45 @@ class MyAuthTest extends StatelessWidget {
   }
 }
 
+final _formKey = GlobalKey<FormState>();
+
 class MySignup extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: Text("signuptestsignuptestsignuptestsignuptestsignuptest")
+        body: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Enter your email',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Validate will return true if the form is valid, or false if
+                    // the form is invalid.
+                    if (_formKey.currentState.validate()) {
+                      // Process data.
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        )
     );
   }
 }
