124ab22bb127acd31ec4f4d71254981580edc04e
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-03-28 14:54:22 +0900

    位置を修正

--- lib/view/auth_view.dart
+++ lib/view/auth_view.dart
@@ -25,12 +25,9 @@ class LoginTopScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         body: ChangeNotifierProvider(
-            create: (context) => CounterStore(), child: LoginScreen()
-        )
-    );
+            create: (context) => CounterStore(), child: LoginScreen()));
   }
 }
-
 
 class LoginScreen extends StatelessWidget {
   Duration get loginTime => Duration(milliseconds: 2250);
@@ -65,9 +62,8 @@ class LoginScreen extends StatelessWidget {
 
   String extractErrorMessage(String original) {
     final exp = RegExp(r'message: (.*)');
-    var list = exp?.allMatches(original)
-        .map((match) => match.group(1))
-        .toList();
+    var list =
+        exp?.allMatches(original).map((match) => match.group(1)).toList();
     if (list.length == 1) {
       return list[0];
     }
@@ -86,23 +82,22 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CounterStore>(
-        builder: (context, counterStore, _) {
-          return FlutterLogin(
-            title: 'ECORP',
-            logo: 'assets/images/ecorp-lightblue.png',
-            onLogin: _authUser,
-            onSignup: _signupUser,
-            onSubmitAnimationCompleted: () {
-              if (authMode == 1) {
-                Navigator.of(context).pushNamed('/afterlogin');
-              } else {
-                Navigator.of(context).pushNamed('/aftersingup');
-              }
-            },
-            onRecoverPassword: _recoverPassword,
-          );
-        });
+    return Consumer<CounterStore>(builder: (context, counterStore, _) {
+      return FlutterLogin(
+        title: 'ECORP',
+        logo: 'assets/images/ecorp-lightblue.png',
+        onLogin: _authUser,
+        onSignup: _signupUser,
+        onSubmitAnimationCompleted: () {
+          if (authMode == 1) {
+            Navigator.of(context).pushNamed('/afterlogin');
+          } else {
+            Navigator.of(context).pushNamed('/aftersingup');
+          }
+        },
+        onRecoverPassword: _recoverPassword,
+      );
+    });
   }
 }
 
@@ -110,9 +105,7 @@ class MyAuthTest extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: Text("login")
-    );
+    return new Scaffold(body: Text("login"));
   }
 }
 
@@ -124,39 +117,41 @@ class MySignup extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         body: Form(
-          key: _formKey,
-          child: Center(
-            child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email',
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    // Validate will return true if the form is valid, or false if
-                    // the form is invalid.
-                    if (_formKey.currentState.validate()) {
-                      // Process data.
-                    }
-                  },
-                  child: Text('Submit'),
-                ),
-              ),
-            ],
-          ),
-        )
-        )
-    );
+            key: _formKey,
+            child: new Container(
+                padding: const EdgeInsets.all(30.0),
+                color: Colors.white,
+                child: new Container(
+                    child: new Center(
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Enter your email',
+                        ),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: RaisedButton(
+                          onPressed: () {
+                            // Validate will return true if the form is valid, or false if
+                            // the form is invalid.
+                            if (_formKey.currentState.validate()) {
+                              // Process data.
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ),
+                    ],
+                  ),
+                )))));
   }
 }
