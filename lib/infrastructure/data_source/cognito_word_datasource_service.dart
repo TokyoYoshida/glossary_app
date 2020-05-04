7390843fef80d10d60eae4ce6a38764d99bce57a
tokyoyoshida <yoshidaforpublic@gmail.com>
2020-05-05 01:06:51 +0900

    AppSyncにつなぎこんだ

--- lib/infrastructure/data_source/cognito_word_datasource_service.dart
+++ lib/infrastructure/data_source/cognito_word_datasource_service.dart
@@ -21,52 +21,52 @@ class CognitoWordDataSourceService implements CognitoApi {
   Future<List<Word>> getAll(ApiSessionSupplier supplier) async {
     supplier.getSession(this);
 
-//    final userPool = new CognitoUserPool(awsUserPoolId, awsClientId);
-//
-//    final credentials = new CognitoCredentials(
-//        'ap-southeast-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', userPool);
-//
-//    await credentials.getAwsCredentials(this._session.getIdToken().getJwtToken());
-//
-//    const endpoint =
-//        'https://xxxxxxxxxxxxxxxxxxxxxxxxxx.appsync-api.ap-southeast-1.amazonaws.com';
-//
-//    final awsSigV4Client = new AwsSigV4Client(
-//        credentials.accessKeyId, credentials.secretAccessKey, endpoint,
-//        serviceName: 'appsync',
-//        sessionToken: credentials.sessionToken,
-//        region: 'ap-southeast-1');
-//
-//    final String query = '''query GetEvent {
-//      getEvent(id: "3dcd52c3-1fd6-4e4d-8da6-946ef4a0c94d") {
-//        id
-//        name
-//        comments(limit: 10) {
-//          items {
-//            content
-//            createdAt
-//          }
-//        }
-//      }
-//    }''';
-//
-//    final signedRequest = new SigV4Request(awsSigV4Client,
-//        method: 'POST', path: '/graphql',
-//        headers: new Map<String, String>.from(
-//            {'Content-Type': 'application/graphql; charset=utf-8'}),
-//        body: new Map<String, String>.from({
-//          'operationName': 'GetEvent',
-//          'query': query}));
-//
-//    http.Response response;
-//    try {
-//      response = await http.post(
-//          signedRequest.url,
-//          headers: signedRequest.headers, body: signedRequest.body);
-//    } catch (e) {
-//      print(e);
-//    }
-//    print(response.body);
+    final userPool = new CognitoUserPool(awsUserPoolId, awsClientId);
+
+    final credentials = new CognitoCredentials(
+        awsUserPoolId, userPool);
+
+    await credentials.getAwsCredentials(this._session.getIdToken().getJwtToken());
+
+    const endpoint =
+        awsAppSyncEndPointUrl;
+
+    final awsSigV4Client = new AwsSigV4Client(
+        credentials.accessKeyId, credentials.secretAccessKey, endpoint,
+        serviceName: 'appsync',
+        sessionToken: credentials.sessionToken,
+        region: 'ap-southeast-1');
+
+    final String query = '''query GetEvent {
+      getEvent(id: "3dcd52c3-1fd6-4e4d-8da6-946ef4a0c94d") {
+        id
+        name
+        comments(limit: 10) {
+          items {
+            content
+            createdAt
+          }
+        }
+      }
+    }''';
+
+    final signedRequest = new SigV4Request(awsSigV4Client,
+        method: 'POST', path: '/graphql',
+        headers: new Map<String, String>.from(
+            {'Content-Type': 'application/graphql; charset=utf-8'}),
+        body: new Map<String, String>.from({
+          'operationName': 'GetEvent',
+          'query': query}));
+
+    http.Response response;
+    try {
+      response = await http.post(
+          signedRequest.url,
+          headers: signedRequest.headers, body: signedRequest.body);
+    } catch (e) {
+      print(e);
+    }
+    print(response.body);
 
     await Future.delayed(Duration(seconds: 1));
     return [
